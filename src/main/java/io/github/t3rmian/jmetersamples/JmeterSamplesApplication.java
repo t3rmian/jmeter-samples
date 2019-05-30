f3ce0894901086d1fd612cef5e259f840e3f9aa7
t3rmian <t3rmian@gmail.com>
2019-05-30 19:28:18 +0200

    initial commit, spring boot 2.1.5 from initializr with java8

---
+++ src/main/java/io/github/t3rmian/jmetersamples/JmeterSamplesApplication.java
@@ -0,0 +1,13 @@
+package io.github.t3rmian.jmetersamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeterSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/t3rmian/jmetersamples/JmeterSamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.t3rmian.jmetersamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmeterSamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

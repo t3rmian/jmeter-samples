ba589545b706f1d14354517297ea198750d9cd96
t3rmian <t3rmian@gmail.com>
2019-06-01 21:38:54 +0200

    Enable h2 in memory db tcp connection

--- src/main/java/io/github/t3rmian/jmetersamples/JmeterSamplesApplication.java
+++ src/main/java/io/github/t3rmian/jmetersamples/JmeterSamplesApplication.java
@@ -1,17 +1,23 @@
 package io.github.t3rmian.jmetersamples;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
+@Configuration
 public class JmeterSamplesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JmeterSamplesApplication.class, args);
     }
 
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+    }
 }

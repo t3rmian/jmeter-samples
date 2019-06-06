0916eaf7add8bc54e5963285f710ec1f763b1747
t3rmian <t3rmian@gmail.com>
2019-06-06 20:44:03 +0200

    Add missing Hibernate Validator dependency

--- src/main/java/io/github/t3rmian/jmetersamples/service/exception/UserExistsException.java
+++
@@ -1,9 +0,0 @@
-package io.github.t3rmian.jmetersamples.service.exception;
-
-import java.text.MessageFormat;
-
-public class UserExistsException extends ClientException {
-    public UserExistsException(long id) {
-        super(MessageFormat.format("User with id: {0} already exists", id));
-    }
-}

af3a7833bc57ae7308411df840f8c2aba33a70d8
t3rmian <t3rmian@gmail.com>
2019-07-01 18:22:56 +0200

    Fix test clash

--- src/test/java/io/github/t3rmian/jmetersamples/controller/RestErrorHandlerTest.java
+++ src/test/java/io/github/t3rmian/jmetersamples/controller/RestErrorHandlerTest.java
@@ -1,23 +1,22 @@
 package io.github.t3rmian.jmetersamples.controller;
 
-import io.github.t3rmian.jmetersamples.JMeterSamplesApplication;
 import io.github.t3rmian.jmetersamples.controller.dto.ErrorResponse;
 import io.github.t3rmian.jmetersamples.data.User;
 import io.github.t3rmian.jmetersamples.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {JMeterSamplesApplication.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class RestErrorHandlerTest {
 
     @Autowired
